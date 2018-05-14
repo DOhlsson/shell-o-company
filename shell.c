f9f9c8fb8ce2fc0a029f058e11611b02f2ebf7d5
Daniel Ohlsson <dohlsson89@gmail.com>
2018-05-14 22:09:31 +0200

    Added first experimental lines of code

---
+++ shell.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int run();
+
+int main(int argc, char* argv[]) {
+	printf("argc %d\n", argc);
+	printf("argv %s\n", argv[0]);
+	char input[255];
+	while (1) {
+		printf("\n> ");
+		scanf("%s", input);
+		printf("input %s\n", input);
+		run(input);
+	}
+
+	return 0;
+}
+
+int run(const char* input) {
+	pid_t pid = fork();
+
+	if (pid > 0) {
+		printf("Waiting for %d\n", pid);
+		int status;
+		waitpid(pid, &status, 0);
+	} else {
+		printf("I am child\n");
+		char* argv[0];
+		int i = execvp(input, argv);
+		printf("i %d\n", i);
+	}
+
+	return 0;
+}
