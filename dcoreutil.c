1cc49c450b9a710742bb6d5cb96e911eb4b501cb
Daniel Lundell <daniellundell91@gmail.com>
2018-05-17 22:21:26 +0200

    Added boiler for coreutil

---
+++ dcoreutil.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "dcoreutil.h"
+
+int is_coreutil_command(const char* text)
+{
+	printf("Hajsan %s\n", text);
+	return 1;
+}
---
+++ dcoreutil.h
@@ -0,0 +1,7 @@
+#ifndef D_CORE_UTIL
+#define D_CORE_UTIL
+
+
+int is_coreutil_command(const char* text);
+
+#endif
